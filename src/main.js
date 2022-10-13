7bad6903de77b14568c1365a4774a08ce99a2a4f
Luiz Rioja <lulyrioja@gmail.com>
2022-10-13 15:12:02 -0300

    chore: fix variable name

--- src/main.js
+++ src/main.js
@@ -1,29 +1,29 @@
-import api from './api';
+import api from "./api";
 
 class App {
   constructor() {
     this.repositories = [];
 
-    this.formEl = document.querySelector('#repo-form');
-    this.listEl = document.querySelector('#repo-list');
-    this.inputEl = document.querySelector('input[name=repository]');
+    this.formEl = document.querySelector("#repo-form");
+    this.listEl = document.querySelector("#repo-list");
+    this.inputEl = document.querySelector("input[name=repository]");
 
     this.registerHandlers();
   }
 
   registerHandlers() {
-    this.formEl.onsubmit = event => this.addRepository(event);
+    this.formEl.onsubmit = (event) => this.addRepository(event);
   }
 
-  setLoanding(loanding = true) {
-    if (loanding === true) {
-      let loandingEl = document.createElement('span');
-      loandingEl.appendChild(document.createTextNode('Carregando'));
-      loandingEl.setAttribute('id', 'loading');
+  setLoading(loading = true) {
+    if (loading === true) {
+      let loadingEl = document.createElement("span");
+      loadingEl.appendChild(document.createTextNode("Carregando"));
+      loadingEl.setAttribute("id", "loading");
 
-      this.formEl.appendChild(loandingEl);
+      this.formEl.appendChild(loadingEl);
     } else {
-      document.getElementById('loading').remove();
+      document.getElementById("loading").remove();
     }
   }
 
@@ -32,15 +32,19 @@ class App {
 
     const repoInput = this.inputEl.value;
 
-    if (repoInput.length === 0)
-      return;
+    if (repoInput.length === 0) return;
 
-    this.setLoanding();
+    this.setLoading();
 
     try {
       const response = await api.get(`/repos/${repoInput}`);
 
-      const { name, description, html_url, owner: { avatar_url } } = response.data;
+      const {
+        name,
+        description,
+        html_url,
+        owner: { avatar_url },
+      } = response.data;
 
       this.repositories.push({
         name,
@@ -49,43 +53,43 @@ class App {
         html_url,
       });
 
-      this.inputEl.value = '';
+      this.inputEl.value = "";
 
       this.render();
     } catch (err) {
-      alert('O repositório não existe!')
+      alert("O repositório não existe!");
     }
 
-    this.setLoanding(false);
+    this.setLoading(false);
   }
 
   render() {
-    this.listEl.innerHTML = '';
+    this.listEl.innerHTML = "";
 
-    this.repositories.forEach(repo => {
-      let imgEl = document.createElement('img');
-      imgEl.setAttribute('src', repo.avatar_url);
+    this.repositories.forEach((repo) => {
+      let imgEl = document.createElement("img");
+      imgEl.setAttribute("src", repo.avatar_url);
 
-      let titleEl = document.createElement('strong');
+      let titleEl = document.createElement("strong");
       titleEl.appendChild(document.createTextNode(repo.name));
 
-      let descriptionEl = document.createElement('p');
+      let descriptionEl = document.createElement("p");
       descriptionEl.appendChild(document.createTextNode(repo.description));
 
-      let linkEl = document.createElement('a');
-      linkEl.setAttribute('target', '_blank');
-      linkEl.setAttribute('href', repo.html_url);
-      linkEl.appendChild(document.createTextNode('Acessar'));
+      let linkEl = document.createElement("a");
+      linkEl.setAttribute("target", "_blank");
+      linkEl.setAttribute("href", repo.html_url);
+      linkEl.appendChild(document.createTextNode("Acessar"));
 
-      let listItemEl = document.createElement('li');
+      let listItemEl = document.createElement("li");
       listItemEl.appendChild(imgEl);
       listItemEl.appendChild(titleEl);
       listItemEl.appendChild(descriptionEl);
       listItemEl.appendChild(linkEl);
 
       this.listEl.appendChild(listItemEl);
-    })
+    });
   }
-};
+}
 
 new App();
